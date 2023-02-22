95dc73d303c0691da9f73537dcd7c4ef97058bff
yarden <topstab4@gmail.com>
2023-02-22 10:41:19 +0000

    finshing modifing

--- output.tf
+++ output.tf
@@ -1,12 +1,11 @@
-output "instance1_ip_addr" {
-  value = aws_instance.yarden-ec2-1.public_ip
+output "instance_ip_addr" {
+  value = [for value in aws_instance.yarden-ec2 : "instance name: ${value.tags.Name} instance IP: ${value.public_ip}"]
 }
-output "instance2_ip_addr" {
-  value = aws_instance.yarden-ec2-2.public_ip
-}
+
 output "instance_Key" {
   value = var.key_name
 }
+
 output "load_balancer_DNS" {
   value = aws_lb.yarden-lb.dns_name
 }
